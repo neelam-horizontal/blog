d8d377d65bc57db3536c3c2d668af06a2ec22741
neelam-horizontal <neelam.ramrakhiani.1@gmail.com>
2023-08-14 00:40:08 +0530

    removed featured

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,6 @@ export default function Home({ posts }) {
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
             <PostWidget />
-            {/* <Categories /> */}
           </div>
         </div>
       </div>
