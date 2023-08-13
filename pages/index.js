2370de44ccfca0e6d2e97408bc5ea41dceac8abd
neelam-horizontal <neelam.ramrakhiani.1@gmail.com>
2023-08-14 00:45:58 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
-import { PostCard, PostWidget } from "../components";
+import { Categories, PostCard, PostWidget } from "../components";
 import { getPosts } from "@/services";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -27,6 +27,7 @@ export default function Home({ posts }) {
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
             <PostWidget />
+            <Categories />
           </div>
         </div>
       </div>
