d8a1f1c69f53e2b082e4da1b0bb4e4dda2f23442
Neelam <neelam.ramrakhiani.1@gmail.com>
2025-08-11 03:08:43 +0530

    removed loader

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { useRouter } from 'next/router';
+import React from "react";
+import { useRouter } from "next/router";
 
-import { getCategories, getCategoryPost } from '../../services';
-import { PostCard, Categories, Loader } from '../../components';
+import { getCategories, getCategoryPost } from "../../services";
+import { PostCard, Categories } from "../../components";
 
 const CategoryPost = ({ posts }) => {
   const router = useRouter();
 
   if (router.isFallback) {
-    return <Loader />;
+    return <></>;
   }
 
   return (
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { useRouter } from 'next/router';
+import React from "react";
+import { useRouter } from "next/router";
 
-import { PostDetail, Categories,
-  //  PostWidget, 
-   Author, Comments, CommentsForm, Loader } from '../../components';
-import { getPosts, getPostDetails } from '../../services';
+import {
+  PostDetail,
+  Categories,
+  //  PostWidget,
+  Author,
+  Comments,
+  CommentsForm,
+} from "../../components";
+import { getPosts, getPostDetails } from "../../services";
 
 const PostDetails = ({ post }) => {
   const router = useRouter();
 
   if (router.isFallback) {
-    return <Loader />;
+    return <></>;
   }
 
   return (
